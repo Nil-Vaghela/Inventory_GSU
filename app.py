b762dc872842fe53df7e1286765b2366d68f7e75
Nil Vaghela <nilrajsinh9@icloud.com>
2024-03-06 17:40:32 -0500

    Changed Location

--- app.py
+++ app.py
@@ -2,13 +2,10 @@ from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('ListToBringDown.html')
 
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
+@app.route('/', methods=['GET','POST'])
+def Run():
+    return render_template("dashboard.html")
 
 @app.route('/add_product')
 def add_product():
