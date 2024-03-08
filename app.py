102ab6509fa11a41d4443726c65a9967c77f4a40
Nil Vaghela <nilrajsinh9@icloud.com>
2024-03-08 00:54:42 -0500

    Design Changes

--- app.py
+++ app.py
@@ -11,6 +11,12 @@ def Run():
     
     return render_template("location.html")
 
+@app.route('/ListToBringDown', methods=['GET','POST'])
+def ListToBringDown():
+    Show_Location = location.Location.ShowLocation()
+    
+    return render_template("ListToBringDown.html")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=8000,debug=True)
