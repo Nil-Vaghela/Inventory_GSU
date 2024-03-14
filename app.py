e344f65983d90be2dc4ce9976dd4730b7e418632
mukhil0212 <138153252+mukhil0212@users.noreply.github.com>
2024-03-14 12:59:03 -0400

    Add a lot of functionality in dashboard

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, redirect, url_for, request
 from Location import location
 from Addstocks import Add_Stocks
 from Database import Temp
+from Addstocks.Add_Stocks import check_stock,total_items
+
 
 app = Flask(__name__)
 
@@ -10,6 +12,16 @@ app = Flask(__name__)
 
 LocationNames = Temp.SessionData.Session("")
 
+
+def update_stock_info():
+    global LocationNames
+    if LocationNames:
+        filepath = LocationNames  # Construct file path dynamically based on location
+        low_stock_threshold = 10  # Define your threshold for low stock
+        out_of_stock, low_stock = check_stock(filepath, low_stock_threshold)
+        return out_of_stock, low_stock
+    else:
+        return [], []
 
 @app.route('/', methods=['GET','POST'])
 def Run():
@@ -20,6 +32,7 @@ def Run():
     return render_template("location.html",file = Show_Location)
 
 
+
 @app.route('/Dashboard',methods = ["GET","POST"])
 def Dashboard():
     if request.method == "POST":
@@ -27,8 +40,12 @@ def Dashboard():
         location_name = location_name[:-5]
         global LocationNames
         LocationNames = Temp.SessionData.Session(location_name)
-        return render_template("dashboard.html",LocationName = LocationNames)
-    return render_template("dashboard.html",LocationName = LocationNames)
+    out_of_stock, low_stock = update_stock_info()
+    total = total_items(LocationNames)
+    return render_template("dashboard.html", LocationName=LocationNames, out_of_stock=out_of_stock, low_stock=low_stock, total=total)
+    
+
+    
 
 @app.route('/AddStocks',methods=["GET","POST"])
 def AddProducts():
@@ -52,6 +69,7 @@ def ListToBringDown():
     Show_Location = location.Location.ShowLocation()
 
     return render_template("ListToBringDown.html")
+
 
 
 if __name__ == '__main__':
